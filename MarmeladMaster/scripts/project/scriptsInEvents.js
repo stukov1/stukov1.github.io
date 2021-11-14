26fc7cadaee1bfe95580ab75b2cb6cf081ab4c10
tanya-ardemasova <94318560+tanya-ardemasova@users.noreply.github.com>
2021-11-14 22:43:06 +0300

    Add files via upload

---
+++ MarmeladMaster/scripts/project/scriptsInEvents.js
@@ -0,0 +1,55 @@
+
+
+
+const scriptsInEvents = {
+
+		async Eads_Event3_Act1(runtime, localVars)
+		{
+			// Show Interstitial ad
+			
+		},
+
+		async Eads_Event4_Act1(runtime, localVars)
+		{
+			// Show Rewarded ad for "get 3 key" button
+		},
+
+		async Eads_Event4_Act2(runtime, localVars)
+		{
+			// call RewardedAdFinished function after rewarded ad watched. (Delete the one below)
+		},
+
+		async Eads_Event5_Act1(runtime, localVars)
+		{
+			// Show Rewarded ad for "get new launcher" button
+		},
+
+		async Eads_Event5_Act2(runtime, localVars)
+		{
+			// call RewardedAdFinished function after rewarded ad watched. (Delete the one below)
+		},
+
+		async Eads_Event6_Act1(runtime, localVars)
+		{
+			// Show Rewarded ad for "Get Free Spin" button
+		},
+
+		async Eads_Event6_Act2(runtime, localVars)
+		{
+			// call RewardedAdFinished function after rewarded ad watched. (Delete the one below)
+		},
+
+		async Eads_Event7_Act1(runtime, localVars)
+		{
+			// Show Rewarded ad for "skiplevel" button
+		},
+
+		async Eads_Event7_Act2(runtime, localVars)
+		{
+			// call RewardedAdFinished function after rewarded ad watched. (Delete the one below)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
